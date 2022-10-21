99b4a80f1dcbe665f5ecf5d79ead06df92495dfc
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-10-21 15:15:34 -0700

    the future is damn bright

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,4 @@
 console.log('time to get tf out');
 console.log('its the last wedding');
 console.log('you wont have to deal with this anymore');
+console.log('just walk away from it all for good');
