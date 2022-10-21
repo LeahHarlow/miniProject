cd0e516f5565043434a8542cd946a2ac0868c03c
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-10-21 15:14:49 -0700

    could always go nuclear

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
 "use strict";
 console.log('time to get tf out');
 console.log('its the last wedding');
+console.log('you wont have to deal with this anymore');
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 console.log('time to get tf out')
 console.log('its the last wedding')
+console.log('you wont have to deal with this anymore')
