d52ca04838025d6f177c8f93aa161e90c01fbd52
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-10-25 11:08:37 -0700

    need to see console logs

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,5 @@ console.log('its the last wedding');
 console.log('you wont have to deal with this anymore');
 console.log('just walk away from it all for good');
 console.log('very great things are in your future');
+console.log("Were gettign there");
+console.log("Were gettign there");
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ console.log('its the last wedding')
 console.log('you wont have to deal with this anymore')
 console.log('just walk away from it all for good')
 console.log('very great things are in your future')
+
+console.log("Were gettign there")
+console.log("Were gettign there")
