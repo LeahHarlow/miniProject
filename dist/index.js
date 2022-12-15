ff1ea3b0b8ba5ed61e72176a22f04fc3ae9ce93f
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-12-14 20:23:28 -0800

    added meta tag for CSP

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+
 console.log('time to get tf out');
 console.log('its the last wedding');
 console.log('you wont have to deal with this anymore');
@@ -6,3 +6,5 @@ console.log('just walk away from it all for good');
 console.log('very great things are in your future');
 console.log("Were gettign there");
 console.log("Were gettign there");
+
+console.log("heyooooo") 
