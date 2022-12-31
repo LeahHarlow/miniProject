51469739666685db7821933519799bd89e77aa91
LeahHarlow <leah.h.b.hamilton@gmail.com>
2022-12-30 21:40:35 -0800

    fixed up prettier and got it working again

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,9 @@
+console.log('time to get tf out')
+console.log('its the last wedding')
+console.log('you wont have to deal with this anymore')
+console.log('just walk away from it all for good')
+console.log('very great things are in your future')
+console.log('Were gettign there')
+console.log('Were gettign there')
 
-console.log('time to get tf out');
-console.log('its the last wedding');
-console.log('you wont have to deal with this anymore');
-console.log('just walk away from it all for good');
-console.log('very great things are in your future');
-console.log("Were gettign there");
-console.log("Were gettign there");
-
-console.log("heyooooo") 
+console.log('heyooooo')
