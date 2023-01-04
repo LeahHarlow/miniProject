a2f1cf0492586c1fadbfbc8943ef2a2447321650
LeahHarlow <leah.h.b.hamilton@gmail.com>
2023-01-03 21:36:39 -0800

    eyy lookin up

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,1 @@
-console.log('time to get tf out')
-console.log('its the last wedding')
-console.log('you wont have to deal with this anymore')
-console.log('just walk away from it all for good')
-console.log('very great things are in your future')
-console.log('Were gettign there')
-console.log('Were gettign there')
-
-console.log('heyooooo')
+console.log("damn lifes pretty fucking beautiful and Im so damn grateful")
